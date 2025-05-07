dc6536015e79817f504f3f881bb647c72cfadff2
chozen <moustafa.eldehni@hotmail.com>
2025-05-07 13:45:00 +0200

    creation card function

---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import "./card.css"
+import nft from "../../public/nft-preview-card-component-main/images/image-equilibrium.jpg"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <div className="picture">
+                <img src="" alt="" />
+            </div>
+            <div className="infos"></div>
+            <div className="author"></div>
+        </div>
+    )
+}
